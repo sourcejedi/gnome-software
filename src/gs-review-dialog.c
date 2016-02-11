a91c1b47dcda83fcd751c1756cb250affc4b3e8d
Robert Ancell <robert.ancell@canonical.com>
2016-02-11 15:00:22 +1300

    Use minimal limits for review content

--- src/gs-review-dialog.c
+++ src/gs-review-dialog.c
@@ -28,11 +28,11 @@
 #include "gs-review-dialog.h"
 #include "gs-star-widget.h"
 
-#define DESCRIPTION_LENGTH_MAX		3000	/* chars */
-#define DESCRIPTION_LENGTH_MIN		60	/* chars */
-#define SUMMARY_LENGTH_MAX		70	/* chars */
-#define SUMMARY_LENGTH_MIN		15	/* chars */
-#define WRITING_TIME_MIN		20	/* seconds */
+#define DESCRIPTION_LENGTH_MAX		10000	/* chars */
+#define DESCRIPTION_LENGTH_MIN		1	/* chars */
+#define SUMMARY_LENGTH_MAX		1000	/* chars */
+#define SUMMARY_LENGTH_MIN		1	/* chars */
+#define WRITING_TIME_MIN		0	/* seconds */
 
 struct _GsReviewDialog
 {
