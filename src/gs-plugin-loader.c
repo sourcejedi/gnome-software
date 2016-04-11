26b03c7fd009ca725c296275ceb9180612e5c203
Richard Hughes <richard@hughsie.com>
2016-04-11 10:31:12 +0100

    trivial: Don't do two update actions for live update

--- src/gs-plugin-loader.c
+++ src/gs-plugin-loader.c
@@ -2628,6 +2628,7 @@ gs_plugin_loader_app_action_async (GsPluginLoader *plugin_loader,
 		gs_plugin_loader_update_async (plugin_loader, list,
 					       cancellable, callback,
 					       user_data);
+		return;
 	}
 
 	if (action == GS_PLUGIN_LOADER_ACTION_REMOVE) {
