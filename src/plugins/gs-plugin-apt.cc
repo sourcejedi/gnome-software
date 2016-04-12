ec4a7e25e2131adbd291af1d560930ea565b643a
Iain Lane <iain@orangesquash.org.uk>
2016-04-12 09:46:09 +0100

    apt: use gs_plugin_order_after

--- src/plugins/gs-plugin-apt.cc
+++ src/plugins/gs-plugin-apt.cc
@@ -76,7 +76,7 @@ gs_plugin_get_name (void)
 }
 
 const gchar **
-gs_plugin_get_deps (GsPlugin *plugin)
+gs_plugin_order_after (GsPlugin *plugin)
 {
 	static const gchar *deps[] = {
 		"appstream",		/* need pkgname */
