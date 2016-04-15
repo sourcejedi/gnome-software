afec556fb3f0bc25f1356256402bfbc2fc7b1c26
William Hua <william.hua@canonical.com>
2016-04-15 09:29:02 -0400

    Add a separate flag for refreshing just the UI

--- src/plugins/gs-plugin-appstream.c
+++ src/plugins/gs-plugin-appstream.c
@@ -76,9 +76,11 @@ gs_plugin_refresh (GsPlugin              *plugin,
 		   GCancellable          *cancellable,
 		   GError               **error)
 {
-	plugin->priv->done_init = FALSE;
-	gs_plugin_appstream_startup (plugin, NULL);
-	gs_plugin_updates_changed (plugin);
+	if (flags & GS_PLUGIN_REFRESH_FLAGS_UI) {
+		plugin->priv->done_init = FALSE;
+		gs_plugin_appstream_startup (plugin, NULL);
+		gs_plugin_updates_changed (plugin);
+	}
 
 	return TRUE;
 }
